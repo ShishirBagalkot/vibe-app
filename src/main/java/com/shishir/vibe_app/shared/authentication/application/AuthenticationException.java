71d5c2d4247db6006d6be0ee906bbea36cf0bc7e
Shishir Bagalkot <59408046+Shishirbagalkot@users.noreply.github.com>
2024-09-21 13:05:19 +0530

    Added auth validation boilerplate

---
+++ src/main/java/com/shishir/vibe_app/shared/authentication/application/AuthenticationException.java
@@ -0,0 +1,3 @@
+package com.shishir.vibe_app.shared.authentication.application;
+
+abstract class AuthenticationException extends RuntimeException {}
---
+++ src/main/java/com/shishir/vibe_app/shared/authentication/application/NotAuthenticatedUserException.java
@@ -0,0 +1,3 @@
+package com.shishir.vibe_app.shared.authentication.application;
+
+public class NotAuthenticatedUserException extends AuthenticationException {}
