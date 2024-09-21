61c4eae2ef9425f6fb46e751826c4563eaa0edbf
Shishir Bagalkot <59408046+Shishirbagalkot@users.noreply.github.com>
2024-09-21 12:13:33 +0530

    Initial commit

---
+++ src/test/java/com/shishir/vibe_app/VibeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shishir.vibe_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
